021546a5b3957bbb23aa4b5dfd4fe99c4015c743
lucasmatto <lucasmatto@gmail.com>
2024-06-14 16:13:12 -0300

    se modifican los archivos DB.js e index.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import Bebida from "./models/Bebida.js";
 export async function database() {
     try {
         await mongoose.connect("mongodb://mongodb:27017/menu", { useNewUrlParser: true, useUnifiedTopology: true });
-
+        await mongoose.connect("mongodb+srv://ezeedge:" + process.env.DBPASS + "@cluster0.ofuzvtm.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Conectado a MongoDB");
     } catch (error) {
         console.error("Error de conexión a MongoDB:", error);
